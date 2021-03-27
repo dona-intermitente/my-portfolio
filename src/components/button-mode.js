59f585f130ebb350a8998c9b57e8eeeefdd96640
dona-intermitente <sindidaconceicao@gmail.com>
2021-03-27 09:54:32 -0400

    modo dark

--- src/components/button-mode.js
+++ src/components/button-mode.js
@@ -1,10 +1,31 @@
-function ButtonMode() {
+import React, { useEffect, useState } from "react";
+
+export default function ButtonMode() {
+    const [checked, setChecked] = useState(
+        localStorage.getItem("class") === "light" ? false : true
+      );
+    useEffect(() => {
+        document
+          .getElementsByTagName("BODY")[0]
+          .setAttribute("class", localStorage.getItem("class"));
+      }, [checked]);
+    const toggleThemeChange = () => {
+        if (checked === true) {
+          localStorage.setItem("class", "light");
+          setChecked(false);
+        } else {
+          localStorage.setItem("class", "dark");
+          setChecked(true);
+        }
+    };
+    console.log(checked)
     return (
-        <label className="switch">
-            <input type="checkbox"></input>
+        <label className={"switch"}>
+            <input type="checkbox"
+            defaultChecked={checked}
+            onChange={() => toggleThemeChange()}
+            />
             <span class="slider text-body"></span>
         </label>
     );
-  }
-  
-export default ButtonMode;
+}
